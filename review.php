8e0d70cfcaf724bb44c0fe3b19265b2fa8c7fd08
Andrew <awalwema@gmail.com>
2016-03-17 14:54:33 -0400

    Changed name of home page to review.php

---
+++ review.php
@@ -0,0 +1,13 @@
+<?php
+session_start();
+echo "Welcome, " . $_SESSION [ 'login_user' ]. " to the review page!" ;
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Review Page</title>
+<link rel="stylesheet" type="text/css" href="signupstyle.css">
+
+</head>
+</html>
